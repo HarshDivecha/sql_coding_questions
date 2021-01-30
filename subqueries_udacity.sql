6d52c0e578f16550046345b9fdd000e6d7ff4da0
Harsh Divecha <harshdivecha@outlook.com>
2021-01-30 00:48:02 -0500

    subqueries

---
+++ subqueries_udacity.sql
@@ -0,0 +1,59 @@
+--  Using Subqueries get the avergae for all types of papers sold in first month when first ever order was placed
+SELECT t3.rep_name, t3.region_name, t3.total_amt
+FROM(SELECT region_name, MAX(total_amt) total_amt
+     FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+             FROM sales_reps s
+             JOIN accounts a
+             ON a.sales_rep_id = s.id
+             JOIN orders o
+             ON o.account_id = a.id
+             JOIN region r
+             ON r.id = s.region_id
+             GROUP BY 1, 2) t1
+     GROUP BY 1) t2
+JOIN (SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+     FROM sales_reps s
+     JOIN accounts a
+     ON a.sales_rep_id = s.id
+     JOIN orders o
+     ON o.account_id = a.id
+     JOIN region r
+     ON r.id = s.region_id
+     GROUP BY 1,2
+     ORDER BY 3 DESC) t3
+ON t3.region_name = t2.region_name AND t3.total_amt = t2.total_amt;
+
+-- Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+
+SELECT region, MAX(total)
+FROM
+	(SELECT s.name "name", r.name "region", SUM(o.total_amt_usd) "total"
+	FROM accounts a
+	JOIN sales_reps s ON a.sales_rep_id = s.id
+	JOIN orders o ON a.id = o.account_id
+	JOIN region r ON r.id = s.region_id
+	GROUP BY 1,2) sub
+GROUP BY 1;
+
+-- For the region with the largest (sum) of sales  , how many total (count) orders were placed?
+
+SELECT r.name, COUNT(o.total) total_orders
+FROM sales_reps s
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id
+JOIN region r
+ON r.id = s.region_id
+GROUP BY r.name
+HAVING SUM(o.total_amt_usd) = (
+      SELECT MAX(total_amt)
+      FROM (SELECT r.name region_name, SUM(o.total_amt_usd) total_amt
+              FROM sales_reps s
+              JOIN accounts a
+              ON a.sales_rep_id = s.id
+              JOIN orders o
+              ON o.account_id = a.id
+              JOIN region r
+              ON r.id = s.region_id
+              GROUP BY r.name) sub);
